72c8d52272957fc9f79fd9680dc2c8c7276b6001
dilhaqued <dilhaqued@gmail.com>
2022-10-08 18:01:17 +0530

    transform to add game history

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [board,setBoard] =useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
-  const handleSquareClick = (position) => {
-    if (board[position]){
-      return;
-    }
-      setBoard ((prev)=> {
-          return prev.map((square,pos)=>{
-            if (pos === position) {
-              return isXNext ? 'x' : '0';
-            }
-            return square;
-          })
-      })
-      setIsXNext(prev => !prev);
-  }
+const Board = ({board, handleSquareClick}) => {
+  
 
   const renderSqaure = position => {
     return (
