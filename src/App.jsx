ca8cc19593fdcfafe599fe72a5468af4ccf4c45c
dilhaqued <dilhaqued@gmail.com>
2022-10-09 00:45:59 +0530

    styling completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React, {useState} from "react";
-import Board from "./components/Board";
-import History from "./components/History";
-import StatusMessage from "./components/StatusMessage";
-import { calculateWinner } from "./helpers";
+import React, { useState } from 'react';
+import Board from './components/Board';
+import History from './components/History';
+import StatusMessage from './components/StatusMessage';
+import { calculateWinner } from './helpers';
+
 import './styles/root.scss';
 
-const NEW_GAME = [
-  { board: Array(9).fill(null), isXNext: true },
-]
+const NEW_GAME = [{ board: Array(9).fill(null), isXNext: true }];
 
 const App = () => {
   const [history, setHistory] = useState(NEW_GAME);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
 
-  const {winner, winningSquares} = calculateWinner(current.board);
- 
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -46,14 +44,29 @@ const App = () => {
   const onNewGame = () => {
     setHistory(NEW_GAME);
     setCurrentMove(0);
-  }
+  };
+
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
-      <StatusMessage winner={winner} current={current}/>
-      <button type="button" onClick={onNewGame}>Start new game</button>
-      <Board board={current.board} handleSquareClick={handleSquareClick} winningSquares={winningSquares}/>
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button
+        type="button"
+        onClick={onNewGame}
+        className={`btn-reset ${winner ? 'active' : ''}`}
+      >
+        Start new game
+      </button>
+      <h2 style={{ fontWeight: 'normal' }}>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 };
