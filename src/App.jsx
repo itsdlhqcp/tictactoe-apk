ad5ba6acb04ac640f60e06e9fdad77272a67571c
dilhaqued <dilhaqued@gmail.com>
2022-10-08 22:00:22 +0530

    transform to add game histor LOGSy

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
+import History from "./components/History";
 import { calculateWinner } from "./helpers";
 import './styles/root.scss';
 
 const App = () => {
-  const [history, setHistory] =useState([{board:Array(9).fill(null), isXNext: true}]);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove];
+
   const winner = calculateWinner(current.board);
-  const message = winner ? `Winner is ${winner}`: `Next player is ${current.isXNext ? 'x' : '0'}`;
-  const handleSquareClick = (position) => {
-    if (current.board[position] || winner){
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (current.board[position] || winner) {
       return;
     }
-      setHistory((prev=> {
-         const last = prev[prev.length-1];
 
-         const newBoard =last.board.map((square,pos) =>{
-          if(pos ===position){
-            return last.isXNext ? 'x': '0';
-          }
-          return square;
-         });
-         return prev.concat({board: newBoard, isXNext: !last.isXNext})
-      }))
-      setCurrentMove(prev => prev + 1);
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove(prev => prev + 1);
   };
+
+  const moveTo = move => {
+    setCurrentMove(move);
+  };
+
   return (
-   <div className="app">
-    <h1>TIC TAC TOE</h1>
-    <h2>{message}</h2>
-    
-    <Board board={current.board} handleSquareClick={handleSquareClick}/>
-   </div>
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <h2>{message}</h2>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
+    </div>
   );
 };
 
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,39 +1,35 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({board, handleSquareClick}) => {
-  
-
-  const renderSqaure = position => {
+const Board = ({ board, handleSquareClick }) => {
+  const renderSquare = position => {
     return (
-    <Square
-     value={board[position]}
-      onClick={() => handleSquareClick(position)}
-    />
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
     );
   };
+
   return (
     <div className="board">
-        <div className="board-row">
-            {/* <Square value={board[0]} onClick={()=>{
-              handleSquareClick(0)
-            }}/> */}
-            { renderSqaure(0)}
-            { renderSqaure(1)}
-            { renderSqaure(2)}
-        </div>
-        <div className="board-row">
-         { renderSqaure(3)}
-         { renderSqaure(4)}
-         { renderSqaure(5)}
-        </div>
-        <div className="board-row">
-        { renderSqaure(6)}
-        { renderSqaure(7)}
-        { renderSqaure(8)}
-        </div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Board
+export default Board;
